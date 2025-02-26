65b73f95c24ac8c246cc4a4693ea06be1caf4307
SnowAce <alexvinh2911@gmail.com>
2025-02-26 08:22:03 +0700

    Finish login and sign up function and fetch with api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/home/Homepages";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        ></Route>
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
       </Routes>
     </>
