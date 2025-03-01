555146af1e306eed556f588c138a559d5c9ad1df
SnowAce <alexvinh2911@gmail.com>
2025-03-01 07:11:57 +0700

    Update UI for the homepage and create DB and update the fetching of api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./pages/home/Homepages";
+import Homepage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
         <Route
           path="/sso-callback"
           element={
@@ -16,7 +17,13 @@ function App() {
             />
           }
         ></Route>
+
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
